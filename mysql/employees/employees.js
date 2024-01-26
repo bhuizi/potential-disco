0af434c4f87eefee48b2e7f9a5d827f1d77059dd
Bryan Huizi <bhuizi@gridrival.com>
2024-01-26 12:00:27 -0800

    - employees adds create - package.json removes ^

--- mysql/employees/employees.js
+++ mysql/employees/employees.js
@@ -13,6 +13,24 @@ export async function list(req, res) {
   }
 }
 
+export async function create(req, res) {
+ const { birth_date, first_name, last_name, gender, hire_date } = req.body;
+ if(!birth_date || !first_name || !last_name || !gender || !hire_date){
+  res.status(400).send("All fields must be provided: birth_date, first_name, last_name, gender, hire_date");
+ }
+ try{
+  const sql = 'INSERT INTO `employees`(`emp_no`, `birth_date`, `first_name`, `last_name`, `gender`, `hire_date` ) VALUES (?, ?, ?, ?, ?, ?)';
+  // uuid would be better | db takes int
+  const emp_no = Math.floor(Math.random() * 100) + 1;
+  const values = [emp_no, '1980-01-01', 'Axl', 'Rose', 'M', '2024-01-25']
+  const [rows, fields] = await connection.execute(sql, values);
+  res.json(rows);
+ } catch(err){
+  console.log(err);
+    res.status(500).send(`Error when posting employee ${err}`);
+ }
+};
+
 export async function read(req, res) {
   const { id } = req.params;
   if (!id) {
--- mysql/router/router.js
+++ mysql/router/router.js
@@ -3,6 +3,7 @@ import * as employees from "../employees/employees.js";
 const router = express.Router();
 
 router.get("/employees", employees.list);
+router.post("/employees", employees.create);
 router.get("/employees/:id", employees.read);
 
 export default router;
